9939492f1ea713fc366ac1c14ae9d8cc2aefcf28
abhishek <abhishek.satapathy01@gmail.com>
2019-06-24 03:13:07 +0530

    fetched datset mnist

--- 02-Classification/classification.py
+++ 02-Classification/classification.py
@@ -1,3 +1,7 @@
+##############
+## Setup
+##############
+
 # Python ≥3.5 is required
 import sys
 assert sys.version_info >= (3, 5)
@@ -32,4 +36,71 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
+
+##############
+## Fetching Dataset
+##############
+
+from sklearn.datasets import fetch_openml
+mnist = fetch_openml('mnist_784', version=1)
+print(mnist.keys())
+
+
+X, y = mnist["data"], mnist["target"]
+X.shape
+y.shape
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28, 28)
+plt.imshow(some_digit_image, cmap=mpl.cm.binary)
+plt.axis("off")
+
+save_fig("some_digit_plot")
+plt.show()
+
+y = y.astype(np.uint8)
+
+def plot_digit(data):
+    image = data.reshape(28, 28)
+    plt.imshow(image, cmap = mpl.cm.binary,
+               interpolation="nearest")
+    plt.axis("off")
+
+# EXTRA
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = mpl.cm.binary, **options)
+    plt.axis("off")
+
+plt.figure(figsize=(9,9))
+example_images = X[:100]
+plot_digits(example_images, images_per_row=10)
+save_fig("more_digits_plot")
+plt.show()
+
+
+#Training and test set
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+
+
+
+##############
+## Binary CLassifier
+##############
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
 
